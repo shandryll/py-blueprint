4180cc2fab7693ad85d7a73e433fb1b23db5e8c8
Shandryll Stephano <shandryll.bibiano.external@renner.com.br>
2025-09-05 10:58:37 -0300

    feature: add vscode folder and remove files

--- src/calculadora.py
+++
@@ -1,3 +0,0 @@
-def soma(a: float, b: float) -> float:
-    """Sum two values."""
-    return a + b
--- tests/test_calculadora.py
+++
@@ -1,8 +0,0 @@
-from src.calculadora import soma
-
-
-def test_soma():
-    """Checks if the sum of two numbers is correct."""
-    assert soma(2, 3) == 5
-    assert soma(-1, 1) == 0
-    assert soma(0, 0) == 0
